5555eb2deaf4cd3224e27614ff12e888f48e37b9
Andrei Mikhailov <amkhlv@gmail.com>
2014-07-20 22:30:21 -0300

    split out headers in wmctrl

--- wmctrl.c
+++ wmctrl.c
@@ -1,3 +1,27 @@
+/* license 
+
+wmjump
+a program for the keyboard navigation of the desktop; 
+use the keyboard to switch between windows and workspaces
+
+Author: Andrei Mikhailov <a.mkhlv@gmail.com>
+
+Copyright (C) 2008
+
+This program is free software which I release under the GNU General Public
+License. You may redistribute and/or modify this program under the terms
+of that license as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+To get a copy of the GNU General Puplic License,  write to the
+Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
 
 /* license {{{ */
 /* 
@@ -25,369 +49,14 @@ Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 /* }}} */
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <locale.h>
-#include <X11/Xlib.h>
-#include <X11/Xatom.h>
-#include <X11/cursorfont.h>
-#include <X11/Xmu/WinUtil.h>
-#include <glib.h>
+#include "wmctrl.h"
 
-#define _NET_WM_STATE_REMOVE        0    /* remove/unset property */
-#define _NET_WM_STATE_ADD           1    /* add/set property */
-#define _NET_WM_STATE_TOGGLE        2    /* toggle property  */
-
-/* help {{{ */
-#define HELP "wmctrl " VERSION "\n" \
-"Usage: wmctrl [OPTION]...\n" \
-"Actions:\n" \
-"  -m                   Show information about the window manager and\n" \
-"                       about the environment.\n" \
-"  -l                   List windows managed by the window manager.\n" \
-"  -d                   List desktops. The current desktop is marked\n" \
-"                       with an asterisk.\n" \
-"  -s <DESK>            Switch to the specified desktop.\n" \
-"  -a <WIN>             Activate the window by switching to its desktop and\n" \
-"                       raising it.\n" \
-"  -c <WIN>             Close the window gracefully.\n" \
-"  -R <WIN>             Move the window to the current desktop and\n" \
-"                       activate it.\n" \
-"  -r <WIN> -t <DESK>   Move the window to the specified desktop.\n" \
-"  -r <WIN> -e <MVARG>  Resize and move the window around the desktop.\n" \
-"                       The format of the <MVARG> argument is described below.\n" \
-"  -r <WIN> -b <STARG>  Change the state of the window. Using this option it's\n" \
-"                       possible for example to make the window maximized,\n" \
-"                       minimized or fullscreen. The format of the <STARG>\n" \
-"                       argument and list of possible states is given below.\n" \
-"  -r <WIN> -N <STR>    Set the name (long title) of the window.\n" \
-"  -r <WIN> -I <STR>    Set the icon name (short title) of the window.\n" \
-"  -r <WIN> -T <STR>    Set both the name and the icon name of the window.\n" \
-"  -k (on|off)          Activate or deactivate window manager's\n" \
-"                       \"showing the desktop\" mode. Many window managers\n" \
-"                       do not implement this mode.\n" \
-"  -o <X>,<Y>           Change the viewport for the current desktop.\n" \
-"                       The X and Y values are separated with a comma.\n" \
-"                       They define the top left corner of the viewport.\n" \
-"                       The window manager may ignore the request.\n" \
-"  -n <NUM>             Change number of desktops.\n" \
-"                       The window manager may ignore the request.\n" \
-"  -g <W>,<H>           Change geometry (common size) of all desktops.\n" \
-"                       The window manager may ignore the request.\n" \
-"  -h                   Print help.\n" \
-"\n" \
-"Options:\n" \
-"  -i                   Interpret <WIN> as a numerical window ID.\n" \
-"  -p                   Include PIDs in the window list. Very few\n" \
-"                       X applications support this feature.\n" \
-"  -G                   Include geometry in the window list.\n" \
-"  -x                   Include WM_CLASS in the window list or\n" \
-"                       interpret <WIN> as the WM_CLASS name.\n" \
-"  -u                   Override auto-detection and force UTF-8 mode.\n" \
-"  -F                   Modifies the behavior of the window title matching\n" \
-"                       algorithm. It will match only the full window title\n" \
-"                       instead of a substring, when this option is used.\n" \
-"                       Furthermore it makes the matching case sensitive.\n" \
-"  -v                   Be verbose. Useful for debugging.\n" \
-"  -w <WA>              Use a workaround. The option may appear multiple\n" \
-"                       times. List of available workarounds is given below.\n" \
-"\n" \
-"Arguments:\n" \
-"  <WIN>                This argument specifies the window. By default it's\n" \
-"                       interpreted as a string. The string is matched\n" \
-"                       against the window titles and the first matching\n" \
-"                       window is used. The matching isn't case sensitive\n" \
-"                       and the string may appear in any position\n" \
-"                       of the title.\n" \
-"\n" \
-"                       The -i option may be used to interpret the argument\n" \
-"                       as a numerical window ID represented as a decimal\n" \
-"                       number. If it starts with \"0x\", then\n" \
-"                       it will be interpreted as a hexadecimal number.\n" \
-"\n" \
-"                       The -x option may be used to interpret the argument\n" \
-"                       as a string, which is matched against the window's\n" \
-"                       class name (WM_CLASS property). Th first matching\n" \
-"                       window is used. The matching isn't case sensitive\n" \
-"                       and the string may appear in any position\n" \
-"                       of the class name. So it's recommended to  always use\n" \
-"                       the -F option in conjunction with the -x option.\n" \
-"\n" \
-"                       The special string \":SELECT:\" (without the quotes)\n" \
-"                       may be used to instruct wmctrl to let you select the\n" \
-"                       window by clicking on it.\n" \
-"\n" \
-"                       The special string \":ACTIVE:\" (without the quotes)\n" \
-"                       may be used to instruct wmctrl to use the currently\n" \
-"                       active window for the action.\n" \
-"\n" \
-"  <DESK>               A desktop number. Desktops are counted from zero.\n" \
-"\n" \
-"  <MVARG>              Specifies a change to the position and size\n" \
-"                       of the window. The format of the argument is:\n" \
-"\n" \
-"                       <G>,<X>,<Y>,<W>,<H>\n" \
-"\n" \
-"                       <G>: Gravity specified as a number. The numbers are\n" \
-"                          defined in the EWMH specification. The value of\n" \
-"                          zero is particularly useful, it means \"use the\n" \
-"                          default gravity of the window\".\n" \
-"                       <X>,<Y>: Coordinates of new position of the window.\n" \
-"                       <W>,<H>: New width and height of the window.\n" \
-"\n" \
-"                       The value of -1 may appear in place of\n" \
-"                       any of the <X>, <Y>, <W> and <H> properties\n" \
-"                       to left the property unchanged.\n" \
-"\n" \
-"  <STARG>              Specifies a change to the state of the window\n" \
-"                       by the means of _NET_WM_STATE request.\n" \
-"                       This option allows two properties to be changed\n" \
-"                       simultaneously, specifically to allow both\n" \
-"                       horizontal and vertical maximization to be\n" \
-"                       altered together.\n" \
-"\n" \
-"                       The format of the argument is:\n" \
-"\n" \
-"                       (remove|add|toggle),<PROP1>[,<PROP2>]\n" \
-"\n" \
-"                       The EWMH specification defines the\n" \
-"                       following properties:\n" \
-"\n" \
-"                           modal, sticky, maximized_vert, maximized_horz,\n" \
-"                           shaded, skip_taskbar, skip_pager, hidden,\n" \
-"                           fullscreen, above, below\n" \
-"\n" \
-"Workarounds:\n" \
-"\n" \
-"  DESKTOP_TITLES_INVALID_UTF8      Print non-ASCII desktop titles correctly\n" \
-"                                   when using Window Maker.\n" \
-"\n" \
-"The format of the window list:\n" \
-"\n" \
-"  <window ID> <desktop ID> <client machine> <window title>\n" \
-"\n" \
-"The format of the desktop list:\n" \
-"\n" \
-"  <desktop ID> [-*] <geometry> <viewport> <workarea> <title>\n" \
-"\n" \
-"\n" \
-"Author, current maintainer: Tomas Styblo <tripie@cpan.org>\n" \
-"Released under the GNU General Public License.\n" \
-"Copyright (C) 2003\n"
-/* }}} */
-
-#define MAX_PROPERTY_VALUE_LEN 4096
-#define SELECT_WINDOW_MAGIC ":SELECT:"
-#define ACTIVE_WINDOW_MAGIC ":ACTIVE:"
 
 #define p_verbose(...) if (options.verbose) { \
     fprintf(stderr, __VA_ARGS__); \
-}
+  }
 
-/* declarations of static functions *//*{{{*/
-static gboolean wm_supports (Display *disp, const gchar *prop);
-static Window *get_client_list (Display *disp, unsigned long *size);
-static int client_msg(Display *disp, Window win, char *msg, 
-        unsigned long data0, unsigned long data1, 
-        unsigned long data2, unsigned long data3,
-        unsigned long data4);
-static int list_windows (Display *disp);
-static int list_desktops (Display *disp);
-static int showing_desktop (Display *disp);
-static int change_viewport (Display *disp);
-static int change_geometry (Display *disp);
-static int change_number_of_desktops (Display *disp);
-static int switch_desktop (Display *disp);
-static int wm_info (Display *disp);
-static gchar *get_output_str (gchar *str, gboolean is_utf8);
-static int action_window (Display *disp, Window win, char mode);
-static int action_window_pid (Display *disp, char mode);
-static int action_window_str (Display *disp, char mode);
-static int activate_window (Display *disp, Window win, 
-        gboolean switch_desktop);
-static int close_window (Display *disp, Window win);
-static int longest_str (gchar **strv);
-static int window_to_desktop (Display *disp, Window win, int desktop);
-static void window_set_title (Display *disp, Window win, char *str, char mode);
-static gchar *get_window_title (Display *disp, Window win);
-static gchar *get_window_class (Display *disp, Window win);
-static gchar *get_property (Display *disp, Window win, 
-        Atom xa_prop_type, gchar *prop_name, unsigned long *size);
-static void init_charset(void);
-static int window_move_resize (Display *disp, Window win, char *arg);
-static int window_state (Display *disp, Window win, char *arg);
-static Window Select_Window(Display *dpy);
-static Window get_active_window(Display *dpy);
-
-/*}}}*/
-   
-static struct {
-    int verbose;
-    int force_utf8;
-    int show_class;
-    int show_pid;
-    int show_geometry;
-    int match_by_id;
-	int match_by_cls;
-    int full_window_title_match;
-    int wa_desktop_titles_invalid_utf8;
-    char *param_window;
-    char *param;
-} options;
-
-static gboolean envir_utf8;
-
-int main (int argc, char **argv) { /* {{{ */
-    int opt;
-    int action = 0;
-    int ret = EXIT_SUCCESS;
-    int missing_option = 1;
-    Display *disp;
-
-    memset(&options, 0, sizeof(options)); /* just for sure */
-    
-    /* necessary to make g_get_charset() and g_locale_*() work */
-    setlocale(LC_ALL, "");
-    
-    /* make "--help" and "--version" work. I don't want to use
-     * getopt_long for portability reasons */
-    if (argc == 2 && argv[1]) {
-        if (strcmp(argv[1], "--help") == 0) {
-            fputs(HELP, stdout);
-            return EXIT_SUCCESS;
-        }
-        else if (strcmp(argv[1], "--version") == 0) {
-            puts(VERSION);
-            return EXIT_SUCCESS;
-        }
-    }
-   
-    while ((opt = getopt(argc, argv, "FGVvhlupidmxa:r:s:c:t:w:k:o:n:g:e:b:N:I:T:R:")) != -1) {
-        missing_option = 0;
-        switch (opt) {
-            case 'F':
-                options.full_window_title_match = 1;
-                break;
-            case 'G':
-                options.show_geometry = 1;
-                break;
-            case 'i':
-                options.match_by_id = 1;
-                break;
-            case 'v':
-                options.verbose = 1;
-                break;
-            case 'u':
-                options.force_utf8 = 1;
-                break;
-			case 'x':
-				options.match_by_cls = 1;
-				options.show_class = 1;
-				break;
-            case 'p':
-                options.show_pid = 1;
-                break;
-            case 'a': case 'c': case 'R':
-                options.param_window = optarg;
-                action = opt;
-                break;
-            case 'r':
-                options.param_window = optarg;
-                break; 
-            case 't': case 'e': case 'b': case 'N': case 'I': case 'T':
-                options.param = optarg;
-                action = opt;
-                break;
-            case 's':
-                options.param = optarg;
-                action = opt;
-                break;
-            case 'w':
-                if (strcmp(optarg, "DESKTOP_TITLES_INVALID_UTF8") == 0) {
-                    options.wa_desktop_titles_invalid_utf8 = 1;
-                }
-                else {
-                    fprintf(stderr, "Unknown workaround: %s\n", optarg);
-                    return EXIT_FAILURE;
-                }
-                break;
-            case 'k': case 'o': case 'n': case 'g':
-                options.param = optarg;
-                action = opt;
-                break;
-            case '?':
-                return EXIT_FAILURE;
-            default:
-                action = opt;
-        }
-    }
-   
-    if (missing_option) {
-        fputs(HELP, stderr);
-        return EXIT_FAILURE;
-    }
-   
-    init_charset();
-    
-    if (! (disp = XOpenDisplay(NULL))) {
-        fputs("Cannot open display.\n", stderr);
-        return EXIT_FAILURE;
-    }
-   
-    switch (action) {
-        case 'V':
-            puts(VERSION);
-            break;
-        case 'h':
-            fputs(HELP, stdout);
-            break;
-        case 'l':
-            ret = list_windows(disp);
-            break;
-        case 'd':
-            ret = list_desktops(disp);
-            break;
-        case 's':
-            ret = switch_desktop(disp);
-            break;
-        case 'm':
-            ret = wm_info(disp);
-            break;
-        case 'a': case 'c': case 'R': 
-        case 't': case 'e': case 'b': case 'N': case 'I': case 'T':
-            if (! options.param_window) {
-                fputs("No window was specified.\n", stderr);
-                return EXIT_FAILURE;
-            }
-            if (options.match_by_id) {
-                ret = action_window_pid(disp, action);
-            }
-            else {
-                ret = action_window_str(disp, action);
-            }
-            break;
-        case 'k':
-            ret = showing_desktop(disp);
-            break;
-        case 'o':
-            ret = change_viewport(disp);
-            break;
-        case 'n':
-            ret = change_number_of_desktops(disp);
-            break;
-        case 'g':
-            ret = change_geometry(disp);
-            break;
-    }
-    
-    XCloseDisplay(disp);
-    return ret;
-}
-/* }}} */
-
-static void init_charset (void) {/*{{{*/
+void init_charset (void) {/*{{{*/
     const gchar *charset; /* unused */
     gchar *lang = getenv("LANG") ? g_ascii_strup(getenv("LANG"), -1) : NULL; 
     gchar *lc_ctype = getenv("LC_CTYPE") ? g_ascii_strup(getenv("LC_CTYPE"), -1) : NULL;
---
+++ wmctrl.h
@@ -0,0 +1,131 @@
+/* license 
+
+wmjump
+a program for the keyboard navigation of the desktop; 
+use the keyboard to switch between windows and workspaces
+
+Author: Andrei Mikhailov <a.mkhlv@gmail.com>
+
+Copyright (C) 2008
+
+This program is free software which I release under the GNU General Public
+License. You may redistribute and/or modify this program under the terms
+of that license as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+To get a copy of the GNU General Puplic License,  write to the
+Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+/* license {{{ */
+/* 
+
+wmctrl
+A command line tool to interact with an EWMH/NetWM compatible X Window Manager.
+
+Author, current maintainer: Tomas Styblo <tripie@cpan.org>
+
+Copyright (C) 2003
+
+This program is free software which I release under the GNU General Public
+License. You may redistribute and/or modify this program under the terms
+of that license as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+To get a copy of the GNU General Puplic License,  write to the
+Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+/* }}} */
+
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+#include <X11/cursorfont.h>
+#include <X11/Xmu/WinUtil.h>
+#include <glib.h>
+
+#define _NET_WM_STATE_REMOVE        0    /* remove/unset property */
+#define _NET_WM_STATE_ADD           1    /* add/set property */
+#define _NET_WM_STATE_TOGGLE        2    /* toggle property  */
+
+
+
+#define MAX_PROPERTY_VALUE_LEN 4096
+#define SELECT_WINDOW_MAGIC ":SELECT:"
+#define ACTIVE_WINDOW_MAGIC ":ACTIVE:"
+
+
+/* declarations of static functions *//*{{{*/
+gboolean wm_supports (Display *disp, const gchar *prop);
+Window *get_client_list (Display *disp, unsigned long *size);
+int client_msg(Display *disp, Window win, char *msg, 
+        unsigned long data0, unsigned long data1, 
+        unsigned long data2, unsigned long data3,
+        unsigned long data4);
+int list_windows (Display *disp);
+int list_desktops (Display *disp);
+int showing_desktop (Display *disp);
+int change_viewport (Display *disp);
+int change_geometry (Display *disp);
+int change_number_of_desktops (Display *disp);
+int switch_desktop (Display *disp);
+int wm_info (Display *disp);
+gchar *get_output_str (gchar *str, gboolean is_utf8);
+int action_window (Display *disp, Window win, char mode);
+int action_window_pid (Display *disp, char mode);
+int action_window_str (Display *disp, char mode);
+int activate_window (Display *disp, Window win, 
+        gboolean switch_desktop);
+int close_window (Display *disp, Window win);
+int longest_str (gchar **strv);
+int window_to_desktop (Display *disp, Window win, int desktop);
+void window_set_title (Display *disp, Window win, char *str, char mode);
+gchar *get_window_title (Display *disp, Window win);
+gchar *get_window_class (Display *disp, Window win);
+gchar *get_property (Display *disp, Window win, 
+        Atom xa_prop_type, gchar *prop_name, unsigned long *size);
+void init_charset(void);
+int window_move_resize (Display *disp, Window win, char *arg);
+int window_state (Display *disp, Window win, char *arg);
+Window Select_Window(Display *dpy);
+Window get_active_window(Display *dpy);
+unsigned long get_timestamp (Display* dsp);
+/*}}}*/
+   
+struct {
+    int verbose;
+    int force_utf8;
+    int show_class;
+    int show_pid;
+    int show_geometry;
+    int match_by_id;
+	int match_by_cls;
+    int full_window_title_match;
+    int wa_desktop_titles_invalid_utf8;
+    char *param_window;
+    char *param;
+} options;
+
+gboolean envir_utf8;
+
+struct timespec wait_time_short;
+struct timespec wait_time_middle;
+struct timespec wait_time_long;
+gboolean verbose;
